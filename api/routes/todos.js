59d9d03b5489344805d3368d65f92d6d970faa27
Csaba Schnitchen <schcsaba@gmail.com>
2023-04-23 13:38:40 +0200

    feat: add nodejs express rest api app

---
+++ api/routes/todos.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const todos = [];
+const router = (0, express_1.Router)();
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    res.status(201).json({ message: 'Todo successfully created', data: newTodo });
+});
+exports.default = router;
